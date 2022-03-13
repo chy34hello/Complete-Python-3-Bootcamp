0c94016bd27448ca7655ad41e9a710157404cc06
chy34hello <57516753+chy34hello@users.noreply.github.com>
2022-03-13 21:42:14 +1100

    Temporary checkin

--- Comp9021/Quiz 2/q2.py
+++ Comp9021/Quiz 2/q2.py
@@ -21,51 +21,49 @@
 # namely, positive numbers (possibly equal to 0).
 
 
-
-
 def binary_lunar_addition(number_1, number_2):
     result = 0
     # INSERT YOUR CODE HERE
-    intToDigits = lambda a : list(map(int, str(a)))
-    stack1 = intToDigits(number_1)
-    stack2 = intToDigits(number_2)
-    factor = 0
-
-    while len(stack1) > 0 or len(stack2) > 0:
-        result += 10**factor * max(stack1.pop() if len(stack1) >
-                              0 else 0, stack2.pop() if len(stack2) > 0 else 0)
-        factor += 1
+    list_n1 =list(reversed(list(map(int, str(number_1)))))
+    list_n2 =list(reversed(list(map(int, str(number_2)))))
+    
+    if number_1 >= number_2:
+      for i in range(len(list_n2)):
+        list_n1[i] = max(list_n1[i], list_n2[i])
+      result = int(''.join([str(i) for i in reversed(list_n1)]))
+    else:
+      for i in range(len(list_n1)):
+        list_n2[i] = max(list_n2[i], list_n1[i])
+      result = int(''.join([str(i) for i in reversed(list_n2)]))
 
     return result
-
 
 def lunar_addition(*numbers):
     result = 0
     # INSERT YOUR CODE HERE
     for n in numbers:
-        result = binary_lunar_addition(result, n)
-
+      result =  binary_lunar_addition(result, n)
+    
     return result
 
 
 def binary_lunar_multiplication(multiplicand, multiplier):
     result = 0
     # INSERT YOUR CODE HERE
-    intToDigits = lambda a : list(map(int, str(a)))
-    m1 = intToDigits (multiplicand)
-    m2 = intToDigits (multiplier)
-    factor = 0
+    multiplicandList = list(map(int, str(multiplicand)))
+    multiplierList = list(map(int, str(multiplier)))
+    tentimes =0
 
-    while m2:
-        partialResult = []
-        m2Digit = m2.pop()
-        for m1Digit in m1:
-            partialResult.append(min(m1Digit, m2Digit))
-
-        currentResult = int(
-            ''.join(list(map(str, partialResult)))) * 10**factor
-        factor += 1
-        result = binary_lunar_addition(result, currentResult)
-
-    return result
+    for multiplierDigit in reversed(multiplierList):
+      currentDigitResultList=[]
+      for multiplicandDigit in multiplicandList:
+        currentDigitResultList.append(min(multiplicandDigit,multiplierDigit))
+      currentResult = int(''.join([str(i) for i in currentDigitResultList])) * 10**tentimes
+      tentimes +=1
+      result = binary_lunar_addition(result, currentResult)
     
+    return result
+
+        
+
+
--- Comp9021/Quiz 3/quiz3.py
+++ Comp9021/Quiz 3/quiz3.py
@@ -1,3 +1,63 @@
+# Written by *** for COMP9021
+#
+# At stage 0, start with a toothpick centered at (0, 0).
+# There is a free tip at the top, at coordinates (0, 2),
+# and a free tip at the bottom, at coordinates (0, -2):
+#
+#         *
+#         *
+#         *
+#         *
+#         *
+#
+# At stage 1, perpendically place toothpicks on those tips.
+# So there are now 3 toothpicks and 4 free tips,
+# a top left one at coordinates (-2, 2),
+# a top right one at coordinates (2, 2),
+# a bottom left one at coordinates (-2, -2), and
+# a bottom right one at coordinates (2, -2):
+#
+#     * * * * *
+#         *
+#         *
+#         *
+#     * * * * *
+#
+# At stage 2, perpendically place toothpicks on those tips.
+# So there are now 7 toothpicks and 4 free tips,
+# a top left one at coordinates (-2, 4),
+# a top right one at coordinates (2, 4),
+# a bottom left one at coordinates (-2, -4), and
+# a bottom right one at coordinates (2, -4):
+#
+#
+#     *       *
+#     *       *
+#     * * * * *
+#     *   *   *
+#     *   *   *
+#     *   *   *
+#     * * * * *
+#     *       *
+#     *       *
+#
+# Implements a function
+# tooth_picks(stage, top_left_corner, bottom_right_corner)
+# that displays, using black and white squares,
+# that part of the plane that fits in a rectangle
+# whose top left and bottom right corners are provided by the
+# second and third arguments, respectively, at the stage of the
+# construction provided by the first argument.
+# You can assume that stage is any integer between 0 and 1000;
+# top_left_corner and bottom_right_corner are arbitrary pairs
+# of integers, but practically such that the output can fit
+# on the screen.
+#
+# For a discussion about the construction, see
+# https://www.youtube.com/watch?v=_UtCli1SgjI&t=66s
+# The video also points to a website that you might find useful:
+# https://oeis.org/A139250/a139250.anim.html
+
 direction_vertical = 'VERTICAL'
 direction_horizontal = 'HORIZONTAL'
 paintBlack = '\N{black large square}'
@@ -6,6 +66,7 @@ toothpickHalfLen = 2
 centerPosition = (0, 0)
 freeTips_tracker = {centerPosition: direction_vertical}
 historical_removed_Tips_tracker = dict()
+howManyRoundsOfRemovedTipsThatIWantToTrack = 4
 
 
 def tooth_picks(stage, top_left_corner, bottom_right_corner):
@@ -22,8 +83,15 @@ def initializeBoard(top_left_corner, bottom_right_corner):
 
 def addMultipleRoundsOfToothspicksToBoard(stage, board):
     while stage:
-        addToothpicksForCurrentRound(board)
+        addToothpicksForCurrentRound(board, stage)
+        # CleanUpUnusedHistoryTracker(stage)
         stage -= 1
+
+
+def CleanUpUnusedHistoryTracker(stage):
+    for key, value in dict(historical_removed_Tips_tracker).items():
+        if value >= stage + howManyRoundsOfRemovedTipsThatIWantToTrack:
+            del historical_removed_Tips_tracker[key]
 
 
 def generateBoard(top_left_corner, bottom_right_corner):
@@ -63,10 +131,10 @@ def constructLine(board, lenOfLine, lineContent, count):
 def addingToothpick(board, coordinate, direction):
     (x, y) = coordinate
 
-    if toothspickFits(board, direction, x, y):
-        if toothpickShouldPaintVertically(direction):
+    # if toothspickFits(board, direction, x, y):
+    if toothpickShouldPaintVertically(direction):
             paintToothspickVertically(board, x, y)
-        else:
+    else:
             paintToothspickHorizontally(board, x, y)
 
 
@@ -76,12 +144,14 @@ def toothpickShouldPaintVertically(direction):
 
 def paintToothspickHorizontally(board, x, y):
     for w in range(x-toothpickHalfLen, x+toothpickHalfLen+1, 1):
-        board[(w, y)] = paintBlack
+        if (w, y) in board:
+            board[(w, y)] = paintBlack
 
 
 def paintToothspickVertically(board, x, y):
     for h in range(y-toothpickHalfLen, y+toothpickHalfLen+1, 1):
-        board[(x, h)] = paintBlack
+        if (x, h) in board:
+            board[(x, h)] = paintBlack
 
 
 def toothspickFits(Matrix, direction, x, y):
@@ -97,24 +167,22 @@ def toothspickFits(Matrix, direction, x, y):
     return toothPickFitsTheboard
 
 
-def addToothpicksForCurrentRound(board):
+def addToothpicksForCurrentRound(board, stage):
 
     for tip in dict(freeTips_tracker):
         (x, y) = tip
-
         if freeTipFacingUpOrDown(tip):
-            removeFreeTipFromTrackerAndMarkTheTip(tip)
             processNewFreeTipCandiate(
-                (x-toothpickHalfLen, y), direction_vertical)
+                (x-toothpickHalfLen, y), direction_vertical, stage)
             processNewFreeTipCandiate(
-                (x+toothpickHalfLen, y), direction_vertical)
+                (x+toothpickHalfLen, y), direction_vertical, stage)
 
         else:
-            removeFreeTipFromTrackerAndMarkTheTip(tip)
             processNewFreeTipCandiate(
-                (x, y-toothpickHalfLen), direction_horizontal)
+                (x, y-toothpickHalfLen), direction_horizontal, stage)
             processNewFreeTipCandiate(
-                (x, y+toothpickHalfLen), direction_horizontal)
+                (x, y+toothpickHalfLen), direction_horizontal, stage)
+        removeFreeTipFromTrackerAndMarkTheTip(tip, stage)
 
     addToothspicksForFreeTips(board)
 
@@ -123,20 +191,22 @@ def freeTipFacingUpOrDown(tip):
     return freeTips_tracker[tip] == direction_horizontal
 
 
-def processNewFreeTipCandiate(FreeTip_candiate, direction):
+def processNewFreeTipCandiate(FreeTip_candiate, direction, stage):
     if FreeTip_candiate in freeTips_tracker:
-        removeFreeTipFromTrackerAndMarkTheTip(FreeTip_candiate)
+        removeFreeTipFromTrackerAndMarkTheTip(FreeTip_candiate, stage)
     elif FreeTip_candiate in historical_removed_Tips_tracker:
         return
     else:
         freeTips_tracker[FreeTip_candiate] = direction
 
 
-def removeFreeTipFromTrackerAndMarkTheTip(tip):
-    historical_removed_Tips_tracker[tip] = freeTips_tracker[tip]
+def removeFreeTipFromTrackerAndMarkTheTip(tip, stage):
+    historical_removed_Tips_tracker[tip] = stage
     freeTips_tracker.pop(tip)
 
 
 def addToothspicksForFreeTips(board):
     for tip in freeTips_tracker:
         addingToothpick(board, tip, freeTips_tracker[tip])
+
+tooth_picks(879, (-67, 89), (-52, 60))
